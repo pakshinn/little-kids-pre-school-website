9b58c54c9776f8bbfbbeb35b6568184b7501fc26
sairevanth0007 <sairevanth0007@gmail.com>
2023-01-11 19:05:58 +0530

    added scroll to top

--- script.js
+++ script.js
@@ -71,4 +71,19 @@ faders.forEach(fader =>{
     appearOnScroll.observe(fader);
 })
 
+//scroll to top 
+let mybutton = document.getElementById("scrolltop");
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
